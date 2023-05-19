5183b5a7686143c4bf3bae81d585b1a1e98ea6bb
Woohyeok Baek <qordngur156@gmail.com>
2023-05-19 21:56:53 +0900

    Refactor : List 레이아웃 리팩토링

--- app/list/components/listContainer/listItem.js
+++ app/list/components/listContainer/listItem.js
@@ -1,16 +1,14 @@
 // scss
-import Image from 'next/image'
 import styles from '../../styles/listItem.module.scss'
 
 
 export default function ListItem({ item }) {
     return (
         <div className={ styles.list_item }>
-                <div className={ styles.image_box }>
-                {/* <Image src="/너굴맨.jpeg" width={100} height={200} alt="너굴맨"/> */}
-                    <img src="/너굴맨.jpeg"/>
-                </div>
-                <p>{item} 이에용</p>
+            <div className={ styles.image_box }>
+                <img src="/너굴맨.jpeg"/>
+            </div>
+            <p>{item} 이에용</p>
         </div>
     )
 }
