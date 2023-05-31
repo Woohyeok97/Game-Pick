8a19e047e270fa51d62e404bf0e9598ca81bed78
Woohyeok Baek <qordngur156@gmail.com>
2023-05-31 18:16:01 +0900

    Style : Menu 컴포넌트 레이아웃 스타일링

--- component/menu/menu.js
+++ component/menu/menu.js
@@ -1,10 +1,24 @@
 import styles from './menu.module.scss'
+import Link from 'next/link'
 
 export default function Menu() {
     return (
         <div className={ styles.menu }>
             <div className={ styles.menu_box }>
-
+                <div className={ styles.menu_nav }>
+                    <div className={ styles.login }>로그인</div>
+                </div>
+                <div className={ styles.link_box }>
+                    <div className={ styles.link }>
+                        <Link href="/">Main</Link>
+                    </div>
+                    <div className={ styles.link }>
+                        <Link href="/list">List</Link>
+                    </div>
+                </div>
+                <div className={ styles.menu_footer }>
+                    <Link href="">Github</Link> / <Link href="">Notion</Link> / <Link href="/admin" className={ styles.admin }>Admin</Link>
+                </div>
             </div>
         </div>
     )
