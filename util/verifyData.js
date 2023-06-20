098ce18d82b3294b3dce029cda01deee6c1f5965
Woohyeok Baek <qordngur156@gmail.com>
2023-06-20 12:29:38 +0900

    Refactor : verifyData 함수 리팩토링

--- util/verifyData.js
+++ util/verifyData.js
@@ -1,5 +1,8 @@
 // 컨텐츠 유효성 검사
 export default function verifyContent(data) {
+    // data가 빈객체 일경우 false 반환
+    if(Object.keys(data).length == 0) return false
+
     for(let i in data) {
         if(i == 'like' || i == 'unlike') continue
         if(!data[i]) return false
