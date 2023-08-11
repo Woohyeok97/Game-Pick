5dd88a1bb095a9d1124846f5d8ac7c24ffb32219
Woohyeok Baek <qordngur156@gmail.com>
2023-08-11 16:57:36 +0900

    Refactor : 피드백 기능 리팩토링(옵티미스틱 업데이트)

--- component/feedback/feedbackIcon.js
+++ component/feedback/feedbackIcon.js
@@ -1,4 +1,5 @@
 'use client'
+import { useEffect } from "react";
 // 커스텀 훅
 import useFeedback from "@/hook임시/feedback/useFeedback";
 // MUI
@@ -8,31 +9,65 @@ import IconButton from '@mui/material/IconButton';
 import ThumbUpIcon from '@mui/icons-material/ThumbUp';
 import ThumbDownIcon from '@mui/icons-material/ThumbDown';
 import Typography from "@mui/material/Typography";
-import { useEffect } from "react";
 
 
 export default function FeedbackIcon({ data }) {
-    const { feedback, setFeedback, fetchPrevFeedback, createFeedback, deleteFeedback } = useFeedback(data)
+    const { feedbackCuont, setFeedbackCount, userFeedback, setUserFeedback, fetchUserFeedback, createFeedback, deleteFeedback } = useFeedback(data)
 
     useEffect(()=>{
-        fetchPrevFeedback()
+        fetchUserFeedback()
     }, [])
-
-    const handleChangeFeedback = async (e) => {
-        
-    }
     
+    
+    const handleChangeFeedback = async (e) => {
+        const name = e.currentTarget.name
+
+        if(!userFeedback) {
+            // 1. 기존 피드백 없을때(피드백 생성)
+            setFeedbackCount((prev) => {
+                return {...prev, [name] : prev[name] + 1}
+            })
+            setUserFeedback({ type : name })
+
+            await createFeedback(name)
+            fetchUserFeedback() 
+            return
+        } else if(userFeedback.type == name){
+            // 2. 기존 피드백과 같은 피드백일때(삭제)
+            setFeedbackCount((prev) => {
+                return {...prev, [name] : prev[name] - 1}
+            })
+            setUserFeedback((prev) => {
+                return {...prev, type : null }
+            })
+            await deleteFeedback()
+            fetchUserFeedback() 
+        } else {
+            // 3. 기존 피드백과 다른 피드백일때(기존피드백 삭제후, 피드백 생성)
+            setFeedbackCount((prev) => {
+                return {...prev, [name] : prev[name] + 1, [userFeedback.type] : prev[userFeedback.type] - 1}
+            })
+            setUserFeedback((prev) => {
+                return {...prev, type : name }
+            })
+            await deleteFeedback()
+            await createFeedback(name)
+            fetchUserFeedback() 
+        }
+    }
     
     return (
         <Box sx={{ display : 'flex' }}>
-            <IconButton size="small" name="like" onClick={ handleChangeFeedback }>
+            <IconButton size="small" name="like" onClick={ handleChangeFeedback }
+             color={ userFeedback && userFeedback.type == "like" ? "primary" : "default" }>
                 <ThumbUpIcon fontSize="inherit" sx={{ mr : '4px' }}/>
-                <Typography>{ feedback.like }</Typography>
+                <Typography>{ feedbackCuont.like }</Typography>
             </IconButton>
             
-            <IconButton size="small" name="dislike" onClick={ handleChangeFeedback }>
+            <IconButton size="small" name="dislike" onClick={ handleChangeFeedback } 
+            color={ userFeedback && userFeedback.type == "dislike" ? "primary" : "default" }>
                 <ThumbDownIcon fontSize="inherit" sx={{ mr : '4px' }}/>
-                <Typography>{ feedback.dislike }</Typography>
+                <Typography>{ feedbackCuont.dislike }</Typography>
             </IconButton>
         </Box>
     )
