01d643b63c7e6b78eb7684835a61389197233a2e
Woohyeok Baek <qordngur156@gmail.com>
2023-08-21 16:28:42 +0900

    Refactor : Carousel로직 커스텀훅 분리

--- app/home/components/homeMain/contentCarousel/index.js
+++ app/home/components/homeMain/contentCarousel/index.js
@@ -1,8 +1,8 @@
 'use client'
-import { useEffect, useState, useRef } from 'react';
 import { CSSTransition } from 'react-transition-group';
 // 커스텀 훅
 import useFetchContent from '@/hook/content/useFetchContent'
+import useCarousel from '@/hook/UI/useCarousel';
 // MUI
 import Box from '@mui/material/Box'
 import Button from '@mui/material/Button'
@@ -10,75 +10,31 @@ import Button from '@mui/material/Button'
 import CarouselItem from './CarouselItem'
 
 
+
 export default function ContentCarousel() {
-    const { fetchContentList } = useFetchContent()
-    const [ contentList, setContentList ] = useState([])
-    // carosel로 보여줄 현재 contentList index값
-    const [ currentIndex, setCurrentIndex ] = useState(0)
-    // carousel 동작여부
-    const [ pause, setPause ] = useState(false);
-
-    // 월요일의 우혁이이게
-    // 3. 그 다음에 useCarousel() 훅 만들어서 로직이랑 분리하자!
-    // 4. 마지막으로 어떤식으로 구현했는지 userRef 중점으로 이해해보자!
-
-    useEffect(()=> {
-        const getContentList = async () => {
-            const result = await fetchContentList('like', 3)
-            setContentList(result)
-        }
-
-        getContentList()
-    }, [])
-
-
-    const intervalRef = useRef(null)
-
-    useEffect(()=> {
-        if(pause) return
-
-        intervalRef.current = setInterval(()=>{
-            setCurrentIndex((prev) => (prev + 1) % contentList.length)
-        }, 2000)
-
-        return () => clearInterval(intervalRef.current)
-    }, [contentList, currentIndex, pause])
-
-    const handleNext = () => {
-        clearInterval(intervalRef.current);
-        setCurrentIndex((prev) => (prev + 1) % contentList.length)
-    }
-    const handlePrev = () => {
-        clearInterval(intervalRef.current);
-        setCurrentIndex((prev) => (prev - 1 + contentList.length) % contentList.length)
-    }
+    const { contentList } = useFetchContent('like', 3)
+    const { caroselIndex, handleNextIndex, handlePrevIndex, handleChangePause } = useCarousel(contentList, 3000)
     
-
     
     return (
         <Box sx={{ flexBasis : '50%', display : 'flex', flexDirection : 'column' }}>
 
-            <Box sx={{ flexBasis : '90%', position : 'relative', overflow : 'hidden' }}
-            onMouseEnter={() => setPause(true)} onMouseLeave={() => setPause(false)}>
-
-                { contentList.map((content, index) => (
-                    <CSSTransition in={currentIndex === index} timeout={300} classNames="slide" key={index} unmountOnExit>
+            <Box onMouseOver={ handleChangePause } onMouseOut={ handleChangePause } sx={{ flexBasis : '90%', position : 'relative', overflow : 'hidden' }}>
+                { contentList.map((content, i) => (
+                    <CSSTransition in={ caroselIndex == i } timeout={3000} classNames="slide" key={content._id} unmountOnExit>
                         <Box sx={{ width: '100%', height: '450px' }}>
-                            <CarouselItem content={content} />
+                            <CarouselItem content={ content } />
                         </Box>
                     </CSSTransition>
                 ))}
-
             </Box>
 
             <Box sx={{ flexBasis : '10%', display : 'flex', justifyContent : 'center', alignItems : 'center' }}>
-                <Button onClick={ handlePrev }>Prev</Button>
+                <Button onClick={ handlePrevIndex }>Prev</Button>
                 <Box>
-                    { contentList.map((_, index) => (
-                            <span key={index} className={index === currentIndex ? 'dot active' : 'dot'}></span>
-                    ))}
+                    { contentList.map((_, i) => <span key={i} className={i == caroselIndex ? 'dot active' : 'dot'}></span> )}
                 </Box>
-                <Button onClick={ handleNext }>Next</Button>
+                <Button onClick={ handleNextIndex }>Next</Button>
             </Box>
 
         </Box>
--- hook/content/useFetchContent.js
+++ hook/content/useFetchContent.js
@@ -1,19 +1,27 @@
 import axios from "axios"
+import { useEffect, useState } from "react"
 
-export default function useFetchContent() {
 
-    const fetchContentList = async (opt, num) => {
+export default function useFetchContent(opt, num) { // opt : 가져올 컨텐츠 기준, num : 가져올 컨텐츠 개수
+    const [ contentList, setContentList ] = useState([])
+
+    useEffect(()=>{
+        fetchContentList(opt, num)
+    }, [])
+
+
+    async function fetchContentList(opt, num){
         try {
             const uri = process.env.NEXT_PUBLIC_CONTENTS_API
             const submission = { sortOption : opt, limit : num }
 
             const response = await axios.get(uri, { params : submission })
-            return response.data.result
+            setContentList(response.data.result)
         } catch(err) {
             console.error(err)
             return
         }
     }
 
-    return { fetchContentList }
+    return { contentList, fetchContentList }
 }
