410119eb56751cc289df3d29b7525fa570dd42d2
Woohyeok Baek <qordngur156@gmail.com>
2023-08-21 17:17:56 +0900

    Style : CaroselItem 스타일

--- app/home/components/homeMain/contentCarousel/CarouselItem.js
+++ app/home/components/homeMain/contentCarousel/CarouselItem.js
@@ -1,6 +1,8 @@
+import Link from 'next/link'
 import { useState } from 'react'
 import styles from '../../../styles/carouselItem.module.scss'
-
+// MUI icons
+import ThumbUpIcon from '@mui/icons-material/ThumbUp';
 
 export default function CarouselItem({ content }) {
     const [ isHovering, setIsHovering ] = useState(false)
@@ -11,7 +13,17 @@ export default function CarouselItem({ content }) {
     return (
         <div className={ styles.carousel_item } onMouseOver={ handleHover } onMouseOut={ handleHover }>
             <img src='/너굴맨.jpeg'/>
-            { isHovering && <div className={ styles.item_info }>{ content.title }</div> }
+
+            { isHovering && 
+            <Link href={`/detail/${content._id}`}>
+                <div className={ styles.item_info }>
+                    <p className={ styles.item_title }>{ content.title }</p>
+                    <div className={ styles.item_like }>
+                        <ThumbUpIcon fontSize="small"/>
+                        <p >{ content.like }</p>
+                    </div>
+                </div>
+            </Link> }
         </div>
     )
 }
