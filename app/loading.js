c18c007ec06071c84cfc05d651643dd886254eea
Woohyeok Baek <qordngur156@gmail.com>
2023-10-13 14:53:52 +0900

    Feat : loading.js 추가

---
+++ app/loading.js
@@ -0,0 +1,7 @@
+export default function Loading() {
+    return (
+        <div className="loading">
+            <img src="/로딩_아이콘.png" title="로딩 바 아이콘  제작자: kerismaker - Flaticon https://www.flaticon.com/kr/free-icons/-"/>
+        </div>
+    )
+}
